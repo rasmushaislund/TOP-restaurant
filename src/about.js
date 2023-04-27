79bbeaca7022ffebec974b5fa9ccefeb3c76d588
Rasmus H <99361520+rasmushaislund@users.noreply.github.com>
2023-04-27 15:09:38 +0200

    Exchange appendChild with append where appropriate

--- src/about.js
+++ src/about.js
@@ -27,15 +27,10 @@ export function createAbout() {
   sectionThreePara.textContent =
     'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.';
 
-  sectionOne.appendChild(sectionOneHeader);
-  sectionOne.appendChild(sectionOnePara);
-  sectionTwo.appendChild(sectionTwoHeader);
-  sectionTwo.appendChild(sectionTwoPara);
-  sectionThree.appendChild(sectionThreeHeader);
-  sectionThree.appendChild(sectionThreePara);
-  about.appendChild(sectionOne);
-  about.appendChild(sectionTwo);
-  about.appendChild(sectionThree);
+  sectionOne.append(sectionOneHeader, sectionOnePara);
+  sectionTwo.append(sectionTwoHeader, sectionTwoPara);
+  sectionThree.append(sectionThreeHeader, sectionThreePara);
+  about.append(sectionOne, sectionTwo, sectionThree);
 
   return about;
 }
